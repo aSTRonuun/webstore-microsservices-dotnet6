a78e91e475e1f0b153c6ab200d09c772e65e118d
Vitor Alves <vitormanoel00710@gmail.com>
2022-07-18 20:00:33 -0300

    fix bug and updated repositorys with base leandro_repository_geekshoping_official

--- s08-eShop-microsservices.NET6-FirstMicrosservices/GeekShooping/GeekShopping.Web/Program.cs
+++ s08-eShop-microsservices.NET6-FirstMicrosservices/GeekShooping/GeekShopping.Web/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddAuthentication(options =>
     options.DefaultScheme = "Cookies";
     options.DefaultChallengeScheme = "oidc";
 })
-    .AddCookie("Cook", c => c.ExpireTimeSpan = TimeSpan.FromMinutes(10))
+    .AddCookie("Cookies", c => c.ExpireTimeSpan = TimeSpan.FromMinutes(10))
     .AddOpenIdConnect("oidc", options =>
     {
         options.Authority = builder.Configuration["ServiceUrls:IdentityServer"];
