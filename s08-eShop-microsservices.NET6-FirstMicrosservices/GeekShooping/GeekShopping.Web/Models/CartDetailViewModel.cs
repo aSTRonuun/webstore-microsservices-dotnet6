432307c5241b1c12274ed577756774ae57f8ff01
Vitor Alves <vitormanoel00710@gmail.com>
2022-08-01 17:51:53 -0300

    Updated project shopping web to added supporte to cartAPI

---
+++ s08-eShop-microsservices.NET6-FirstMicrosservices/GeekShooping/GeekShopping.Web/Models/CartDetailViewModel.cs
@@ -0,0 +1,12 @@
+﻿using GeekShopping.Web.Models;
+
+namespace GeekShpping.Web.Models;
+public class CartDetailViewModel
+{
+    public long CartHeaderId { get; set; }
+    public CartHeaderViewModel CartHeader { get; set; }
+
+    public long ProductId { get; set; }
+    public ProductViewModel Product { get; set; }
+    public int Count { get; set; }
+}
