92ca02b4736725a933f320f124ccb043b1cd313f
Vitor Alves <vitormanoel00710@gmail.com>
2022-08-19 12:02:00 -0300

    added new microsservice and updating configures in new microsservice

---
+++ s08-eShop-microsservices.NET6-FirstMicrosservices/GeekShooping/GeekShopping.OderAPI/Model/OrderDetail.cs
@@ -0,0 +1,21 @@
+﻿using GeekShopping.OderAPI.Model.Base;
+using GeekShpping.OrderAPI.Model;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShpping.OderAPI.Model;
+
+[Table("order_detail")]
+public class OrderDetail : BaseEntity
+{
+    public long CartHeaderId { get; set; }
+
+    [ForeignKey("CartHeaderId")]
+    public virtual CartHeader? CartHeader { get; set; }
+
+    public long ProductId { get; set; }
+    [ForeignKey("ProductId")]
+    public virtual Product Product { get; set; }
+
+    [Column("count")]
+    public int Count { get; set; }
+}
---
+++ s08-eShop-microsservices.NET6-FirstMicrosservices/GeekShooping/GeekShopping.OderAPI/Model/OrderHeader.cs
@@ -0,0 +1,14 @@
+﻿using GeekShopping.OderAPI.Model.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShpping.OrderAPI.Model;
+
+[Table("order_header")]
+public class OrderHeader : BaseEntity
+{
+    [Column("user_id")]
+    public string UserId { get; set; }
+    
+    [Column("coupon_code")]
+    public string? CouponCode { get; set; }
+}
